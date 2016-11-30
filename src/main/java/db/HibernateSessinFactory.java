ddf39cf9428872d282145dfc7685ea5ae59ea424
hign <llsmpsvn@gmain.com>
2016-11-30 23:36:33 +0800

    Initial commit

---
+++ src/main/java/db/HibernateSessinFactory.java
@@ -0,0 +1,9 @@
+package db;
+
+/**
+ * Created by hign on 2016/11/30.
+ */
+public class HibernateSessinFactory {
+
+    private 
+}
---
+++ src/main/java/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by hign on 2016/11/30.
+ */
+package service;
